7d764a70c0c6a679ec882f631737126b4ce1406d
Pedro Romero <pedro.romero@bluestart.mx>
2021-04-13 12:26:45 -0500

    subject: name of covariable and cells reports

--- epi_puma_1/reports.py
+++ epi_puma_1/reports.py
@@ -31,8 +31,13 @@ def nbayes_report(path, target, covariables, lim_inf_training, lim_sup_training,
             - absolute path cells report 
             - message
     '''
+
+    covariable_filename = 'COVARIABLES::'
+    cells_filename = 'CELLS::'
+
     try:
-        absolute_path = os.path.join(os.path.dirname(__file__), path)
+        absolute_path = os.path.join(os.path.dirname(__file__), '../')
+        absolute_path = os.path.join(absolute_path, path)
         print(absolute_path)
     except Exception as e:
         message = "There was a problem related path of the report"
@@ -42,11 +47,50 @@ def nbayes_report(path, target, covariables, lim_inf_training, lim_sup_training,
         lim_inf_training, lim_sup_training, lim_inf_validation, lim_sup_validation, 
         modifier, type_analysis, lim_inf_first, lim_sup_first)
 
+
+    covariable_filename += 'target:' 
+    cells_filename += 'target:'
+    for t in target:
+        covariable_filename += t + ';'
+        cells_filename += t + ';'
+
+    covariable_filename += ';'
+    cells_filename += ';'
+
+    covariable_filename += 'covariables:' 
+    cells_filename += 'covariables:' 
+    for c in covariables:
+        covariable_filename += c + ';'
+        cells_filename += c + ';'
+
+    covariable_filename += ';'
+    cells_filename += ';'
+
+    covariable_filename += 'training:' + lim_inf_training + '_to_' + lim_sup_training 
+    cells_filename += 'training:' + lim_inf_training + '_to_' + lim_sup_training
+
+    covariable_filename += 'validation:' + lim_inf_validation + '_to_' + lim_sup_validation 
+    cells_filename += 'validation:' + lim_inf_validation + '_to_' + lim_sup_validation
+
+    if type_analysis == None:
+        covariable_filename += 'type:profiling'
+        cells_filename += 'type:profiling'
+    else:
+        covariable_filename += 'type:' + type_analysis
+        cells_filename += 'type:' + type_analysis
+
+    if modifier == None:
+        covariable_filename += 'modifier:' + modifier
+        cells_filename += 'modifier:' + modifier
+
+    covariable_filename += '.csv'
+    cells_filename += '.csv'
+
     df_covariables = pd.DataFrame(covariables_list)
     df_cells = pd.DataFrame(summary_list)
 
-    df_covariables.to_csv(os.path.join(absolute_path, 'covars.csv'), index=False)
-    df_cells.to_csv(os.path.join(absolute_path, 'cells.csv'), index=False)
+    df_covariables.to_csv(os.path.join(absolute_path, covariable_filename), index=False)
+    df_cells.to_csv(os.path.join(absolute_path, cells_filename), index=False)
 
 
 
